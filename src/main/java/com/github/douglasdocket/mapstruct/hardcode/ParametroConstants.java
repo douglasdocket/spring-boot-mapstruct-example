5b9ce48900f2525cee7ac21df7a1eb9d127dadd3
Douglas Miguel <douglas.miguel@docket.com.br>
2018-09-17 19:54:13 -0300

    melhoria de codigo

---
+++ src/main/java/com/github/douglasdocket/mapstruct/hardcode/ParametroConstants.java
@@ -0,0 +1,8 @@
+package com.github.douglasdocket.mapstruct.hardcode;
+
+public class ParametroConstants {
+	public static final String PARAMETRO_NOVA_EMPRESA = "novaEmpresa";
+
+	private ParametroConstants() {
+	}
+}
